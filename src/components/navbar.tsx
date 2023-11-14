2388f967588a6d35661c496dd6b90b095fb9afc7
juniardys <juniardysetio@gmail.com>
2023-11-14 17:53:47 +0700

    feat: add search input

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -24,7 +24,7 @@ export const Navbar = () => {
             "hidden md:block text-xl md:text-3xl font-bold text-primary",
             font.className
           )}>
-            companion.ai
+            AI Companion
           </h1>
         </Link>
       </div>
